4aa6cb8a1f43f50a4d3f46399b43a422aea54647
Robin Muccari <remuccari@gmail.com>
2018-01-16 19:43:02 -0500

    Update line-chart-transition-script.js

--- line-chart-transition-script.js
+++ line-chart-transition-script.js
@@ -6,25 +6,25 @@ var margin = {left:50,right:50,top:80,bottom:0};
 var parseDate = d3.timeParse("%Y");
 
 var x = d3.scaleTime()
-            .range([0,width]);
+         .range([0,width]);
 
 var y = d3.scaleLinear()
-            .range([height,0]);
+        .range([height,0]);
 
 var yAxis = d3.axisLeft(y)
-			.ticks(3)
-			.tickPadding(10)
-			.tickSize(7);                        
+	.ticks(3)
+	.tickPadding(10)
+	.tickSize(7);                        
 
 var xAxis = d3.axisBottom(x)
-			.ticks(5)
-			.tickPadding(10)
-			.tickSize(7)
-			.tickSizeOuter(0); //tickSizeOuter removes the tick at 0
+	.ticks(5)
+	.tickPadding(10)
+	.tickSize(7)
+	.tickSizeOuter(0);
 
 var line = d3.line()
-			.x(function(d,i){ return x(d.year); })
-			.y(function(d,i){ return y(d.amount); });
+	.x(function(d,i){ return x(d.year); })
+	.y(function(d,i){ return y(d.amount); });
 
 function transition(path) {
   path.transition()
@@ -40,46 +40,31 @@ function tweenDash() {
 }
 
 var svg = d3.select("#area-chart")
-			.append("svg")
-			.attr("height",height + margin.top)
-			.attr("width",width + margin.left);
-
-// gridlines in x axis function
-/*function make_x_gridlines() {		
-    return d3.axisBottom(x)
-        .ticks(5)
-}*/
+	.append("svg")
+	.attr("height",height + margin.top)
+	.attr("width",width + margin.left);
 
 // gridlines in y axis function
 function make_y_gridlines() {		
     return d3.axisLeft(y)
-        .ticks(4)
+             .ticks(4)
 }
 
 d3.csv("craft-stats.csv", function(error, data) {
       if (error) throw error;
 
      data.forEach(function(d) {
-	      d.year = parseDate(d.year);
-	      d.amount = +d.amount;
+	  d.year = parseDate(d['Year']);
+          d.amount = +(d['Amount']);
      });
 
 
 	x.domain(d3.extent(data,function(d){ return d.year; }));
-	         
+
 	y.domain([0, d3.max(data, function(d){ return d.amount; })]);
 
 	var chartGroup = svg.append("g")
-				.attr("transform","translate(50,-5)");
-
-	  // X gridlines (verticle)
-	  /*chartGroup.append("g")			
-	      .attr("class", "grid")
-	      .attr("transform", "translate(0," + height + ")")
-	      .call(make_x_gridlines()
-	          .tickSize(-height)
-	          .tickFormat("")
-	      )*/
+		.attr("transform","translate(50,-5)");
 
 	  // Y gridlines (horizontal)
 	  chartGroup.append("g")			
@@ -87,28 +72,27 @@ d3.csv("craft-stats.csv", function(error, data) {
 	      .attr('stroke-dasharray',('3,3'))
 	      .attr('stroke-width',0.5)
 	      .call(make_y_gridlines()
-	          .tickSize(-width)
-	          .tickFormat("")
+		  .tickSize(-width)
+		  .tickFormat("")
 	      )
 
 	chartGroup.append("g")
-		      .attr("class","axis y")
-		      .call(yAxis);
+	      .attr("class","axis y")
+	      .call(yAxis);
 
 	chartGroup.append("g")
-		      .attr("class","axis x")
-		      .attr('transform','translate(0,' + height + ')')
-		      .call(xAxis);
+	      .attr("class","axis x")
+	      .attr('transform','translate(0,' + height + ')')
+	      .call(xAxis);
 
 	chartGroup.append("path")
-				.attr("d",line(data))
-				.attr('class','line')
-				.attr('fill','none')
-				.style('stroke','black')
-				.style('stroke-width','2')
-				.call(transition);
-	               
-
+		.attr("d",line(data))
+		.attr('class','line')
+		.attr('fill','none')
+		.style('stroke','black')
+		.style('stroke-width','2')
+		.call(transition);
+	              
 });
 
 
